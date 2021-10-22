7f213e9c8013954a8e665bb995f00dad020492b2
calvirick <86133577+calvirick@users.noreply.github.com>
2021-10-22 16:33:47 -0700

    Pretty up Interface
    "Cabinet Sign Parameters:" was made to look nice, and balanced out with the mobile version.

--- root/js/mainFunctions.js
+++ root/js/mainFunctions.js
@@ -1,9 +1,11 @@
+/*Code Property of Alex Wilkins. All rights reserved 2021 Oct-22nd-21 */
+
 $("#calculator").submit(function (event) {
   event.preventDefault();
   let lengthInFeet = $("#lengthInFeet").val();
   let numberOfRows = $("#numberOfRows").val();
   let modulesFor1stRow = lengthInFeet * 4;
   let totalModules = modulesFor1stRow * numberOfRows;
-  let result = totalModules / 75;
+  let result = (Math.ceil(totalModules * 10) / 10).toFixed(1) / 75;
   $("#output").text(result);
 });
